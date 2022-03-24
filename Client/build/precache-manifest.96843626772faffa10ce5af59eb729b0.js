fc43a1c96e08ab88843acfe7777e882b16aa96b8
prahyas <xrestha17@gmail.com>
2022-03-24 18:20:52 +0545

    final working version of restro app with server.

---
+++ Client/build/precache-manifest.96843626772faffa10ce5af59eb729b0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "391bdfe37e829574cf5ef4f0bfabdf6f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53290cd470dfa008d0c8",
+    "url": "/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "c22191f960a04c5f4b05",
+    "url": "/static/css/main.9470a96b.chunk.css"
+  },
+  {
+    "revision": "53290cd470dfa008d0c8",
+    "url": "/static/js/2.9ee5a4d3.chunk.js"
+  },
+  {
+    "revision": "16b5696b7edbc775e112f8d1551b3e7e",
+    "url": "/static/js/2.9ee5a4d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c22191f960a04c5f4b05",
+    "url": "/static/js/main.afa7ed47.chunk.js"
+  },
+  {
+    "revision": "cefe2585034291da62f4",
+    "url": "/static/js/runtime-main.cc02cbba.js"
+  },
+  {
+    "revision": "7fddffdd8cfd405a230d46657003cda1",
+    "url": "/static/media/Food2.7fddffdd.png"
+  }
+]);
